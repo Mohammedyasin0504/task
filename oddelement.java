923bab1843357b02e9fa5e596096a45f66daada4
Mohammedyasin0504 <124041993+Mohammedyasin0504@users.noreply.github.com>
2023-02-22 20:24:44 +0530

    Add files via upload

---
+++ oddelement.java
@@ -0,0 +1,15 @@
+public class oddelement
+{
+public static void main (String args[])
+{
+int a[]={12,10,2,5,8,9};
+for(int i=0;i<=a.length;i=i+1)
+{
+if(a[i]%2==1)
+System.out.println(a[i]);
+
+}
+
+}
+
+}
